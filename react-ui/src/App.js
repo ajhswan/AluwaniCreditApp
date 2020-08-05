e2bb844918b47797dfc8c4f7b06ddf7dfb33d56b
Andrew Swan <andrew@Mac.local>
2020-08-05 15:30:19 +0100

    Added hotloading

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
+import { hot } from 'react-hot-loader';
 import './App.css';
 
 class App extends Component {
     render(){
         return(
             <div className = 'App'>
-                <h1>Hello, REACT</h1>
+                <h1>Hello, HotLoadingTry Again</h1>
             </div>
         );
     }
 }
 
-export default App;
+export default hot(module)(App);
