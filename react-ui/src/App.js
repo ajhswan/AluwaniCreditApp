0255674b351aa1d2c48df15b9b84c092d46325b7
Andrew Swan <andrew@Mac.local>
2020-08-05 15:48:01 +0100

    reinstalled npm packages to fix module error

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
     render(){
         return(
             <div className = 'App'>
-                <h1>Hello, HotLoadingTry Again</h1>
+                <h1>Hello, HotLoadingTry Again and again</h1>
             </div>
         );
     }
